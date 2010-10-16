01dabcce10f05cc07cdb827a07d9ea4bcdbed8c6
Michael Hampton <mirhampt@gmail.com>
2010-10-16 17:46:17 -0400

    Add example express app to tree

---
+++ examples/express_test.js
@@ -0,0 +1,48 @@
+var express  = require('express'),
+    Recaptcha = require('recaptcha').Recaptcha;
+
+var PUBLIC_KEY  = 'YOUR_PUBLIC_KEY',
+    PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
+
+var app = express.createServer();
+
+app.configure(function() {
+    app.use(express.bodyDecoder());
+});
+
+app.get('/', function(req, res) {
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
+
+    res.render('form.jade', {
+        layout: false,
+        locals: {
+            recaptcha_form: recaptcha.toHTML()
+        }
+    });
+});
+
+app.post('/', function(req, res) {
+    var data = {
+        remoteip:  req.connection.remoteAddress,
+        challenge: req.body.recaptcha_challenge_field,
+        response:  req.body.recaptcha_response_field
+    };
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
+
+    recaptcha.verify(function(success, error_code) {
+        if (success) {
+            res.send('Recaptcha response valid.');
+        }
+        else {
+            // Redisplay the form.
+            res.render('form.jade', {
+                layout: false,
+                locals: {
+                    recaptcha_form: recaptcha.toHTML()
+                }
+            });
+        }
+    });
+});
+
+app.listen(3000);
