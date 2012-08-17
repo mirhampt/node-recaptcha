aec21c46b559ff0e4eae5119c63dd4ea19eb286a
Robert Kowalski <rok@kowalski.gd>
2012-08-17 11:44:13 +0200

    make example runnable with express 2.5.x

--- examples/express_test.js
+++ examples/express_test.js
@@ -1,19 +1,40 @@
-var express  = require('express'),
-    Recaptcha = require('recaptcha').Recaptcha;
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    Recaptcha = require('../lib/recaptcha').Recaptcha;
 
 var PUBLIC_KEY  = 'YOUR_PUBLIC_KEY',
     PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
 
-var app = express.createServer();
 
-app.configure(function() {
-    app.use(express.bodyParser());
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
 });
 
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
 app.get('/', function(req, res) {
     var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
 
-    res.render('form.jade', {
+    res.render('form', {
         layout: false,
         locals: {
             recaptcha_form: recaptcha.toHTML()
@@ -35,7 +56,7 @@ app.post('/', function(req, res) {
         }
         else {
             // Redisplay the form.
-            res.render('form.jade', {
+            res.render('form', {
                 layout: false,
                 locals: {
                     recaptcha_form: recaptcha.toHTML()
@@ -45,4 +66,6 @@ app.post('/', function(req, res) {
     });
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
